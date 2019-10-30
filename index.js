499e4bf1c402ad790ead348d26be27cc3bacc14f
Tai <tmtudev@gmail.com>
2019-10-30 17:45:49 +0700

    update file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const io = socketIO(server);
 
 var connector = 0;
 var startFunction = setInterval(initRoom, 100);
-setInterval(update, 10000); //time is in ms
+// setInterval(update, 10000); //time is in ms
 io.attach(4567);
 
 var joinedRoom;
@@ -31,15 +31,15 @@ var minRoom = 20000;
 var maxRoom = 100000;
 
 
-function update(){
-	logInfo();
-}
+// function update(){
+// 	logInfo();
+// }
 
-function logInfo(){
-	var log = "Player connecting: " + connector;
-	io.emit('msg', log);
-	console.log(log);
-}
+// function logInfo(){
+// 	var log = "Player connecting: " + connector;
+// 	io.emit('msg', log);
+// 	console.log(log);
+// }
 
 
 
@@ -56,109 +56,115 @@ io.on('connection', function(socket){
 	connector++;
 	console.log("socket connecting: " + socket.id);
 	onSocketConnecting(socket);
-});
-
-const maxPlayer = 4;
-
-function join(socket, room){
-			socket.join(room, function(){
-			joinedRoom = room;
-			rooms[room]++;
-			socket.emit("joinRoom", {roomId: room});
-			console.log("socket is joined room: " + room)
-			console.log("total player in " + room +  " is: " + rooms[room]);
-
-			if(rooms[room] == maxPlayer){
-				//start game
-				console.log("room " + room + " starting game!");
-				socket.emit("startGame");
-
-			}
-		});
-}
-
-function joinRoom(socket, room){
-
-		// var cr = io.sockets.clients(room);
-		// if(typeof cr === "undefined"){
-		if(rooms[room] <= 0){
-			//no room, just join.
-			console.log("no player, just join this room and own it!");
-			join(socket, room);
-
-			// var cr =  io.sockets.clients(room);
-
-		}
-		else{
-		if(rooms[room] < maxPlayer){
-			console.log("lenme join the party!");
-			join(socket, room);
-		}
-		else{
-			socket.emit("roomFull");
-			console.log("room: " + room + " is full!");
-		}
-	}
-
-
-}
-
-function getRandomRoom(){
-	var r = Math.random();
-		var min = minRoom;
-		var max = maxRoom;
-		var factor = ((1 - r) * min) + (r * max);
-
-		return  Math.floor(factor);
-}
-
-function createRoom(socket){
-	console.log("Creating new room...");
-		var room = getRandomRoom();
-
-		if(rooms[room] >= 1){
-			console.log("room exist, create other room!");
-			createRoom();	
-			return;
-
-		}
-		// var crom =  io.sockets.clients(room);
-		// if(typeof crom !== "undefined"){
-		// //room exist, create another one.
-		// 	createRoom();
-		// 	return;
-		// }
-		joinRoom(socket, room);
-
-}
-
-function onSocketConnecting(socket){
-	socket.on('createRoom', function(){
-		createRoom(socket);
-	});
 
 	socket.on('disconnect', (reason) => {
 		console.log("some one disconnect!");
-	connector--;
-	socket.emit("exitRoom");
-	var rd = roomDatas[socket.id] 
-	if(typeof rd !== "undefined"){
-		//disconnect from room
-		joinedRoom = "undefined";
-		rooms[rd]--;
-		console.log("Some one leave room " + rd + " reason: " + reason);
-		console.log("total player in " + rd +  " is: " + rooms[rd]);
+
 	}
+});
+
+
+const maxPlayer = 4;
+
+// function join(socket, room){
+// 			socket.join(room, function(){
+// 			joinedRoom = room;
+// 			rooms[room]++;
+// 			socket.emit("joinRoom", {roomId: room});
+// 			console.log("socket is joined room: " + room)
+// 			console.log("total player in " + room +  " is: " + rooms[room]);
+
+// 			if(rooms[room] == maxPlayer){
+// 				//start game
+// 				console.log("room " + room + " starting game!");
+// 				socket.emit("startGame");
+
+// 			}
+// 		});
+// }
+
+// function joinRoom(socket, room){
+
+// 		// var cr = io.sockets.clients(room);
+// 		// if(typeof cr === "undefined"){
+// 		if(rooms[room] <= 0){
+// 			//no room, just join.
+// 			console.log("no player, just join this room and own it!");
+// 			join(socket, room);
+
+// 			// var cr =  io.sockets.clients(room);
+
+// 		}
+// 		else{
+// 		if(rooms[room] < maxPlayer){
+// 			console.log("lenme join the party!");
+// 			join(socket, room);
+// 		}
+// 		else{
+// 			socket.emit("roomFull");
+// 			console.log("room: " + room + " is full!");
+// 		}
+// 	}
+
+
+// }
+
+// function getRandomRoom(){
+// 	var r = Math.random();
+// 		var min = minRoom;
+// 		var max = maxRoom;
+// 		var factor = ((1 - r) * min) + (r * max);
+
+// 		return  Math.floor(factor);
+// }
+
+// function createRoom(socket){
+// 	console.log("Creating new room...");
+// 		var room = getRandomRoom();
+
+// 		if(rooms[room] >= 1){
+// 			console.log("room exist, create other room!");
+// 			createRoom();	
+// 			return;
+
+// 		}
+// 		// var crom =  io.sockets.clients(room);
+// 		// if(typeof crom !== "undefined"){
+// 		// //room exist, create another one.
+// 		// 	createRoom();
+// 		// 	return;
+// 		// }
+// 		joinRoom(socket, room);
+
+// }
+
+// function onSocketConnecting(socket){
+// 	socket.on('createRoom', function(){
+// 		createRoom(socket);
+// 	});
+
+// 	socket.on('disconnect', (reason) => {
+// 		console.log("some one disconnect!");
+// 	connector--;
+// 	socket.emit("exitRoom");
+// 	var rd = roomDatas[socket.id] 
+// 	if(typeof rd !== "undefined"){
+// 		//disconnect from room
+// 		joinedRoom = "undefined";
+// 		rooms[rd]--;
+// 		console.log("Some one leave room " + rd + " reason: " + reason);
+// 		console.log("total player in " + rd +  " is: " + rooms[rd]);
+// 	}
 	 
-	});
+// 	});
 
-	socket.on("joinRoom", function(roomId){
-		console.log("received event from socket: " + socket.id);
-		roomDatas[socket.id] = roomId;
-		joinRoom(socket, roomId);
-	});
+// 	socket.on("joinRoom", function(roomId){
+// 		console.log("received event from socket: " + socket.id);
+// 		roomDatas[socket.id] = roomId;
+// 		joinRoom(socket, roomId);
+// 	});
 
-}
+// }
 
 
 
