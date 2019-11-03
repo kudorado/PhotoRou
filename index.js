e5f58015cdf66b0a723e80a03f457f5a35cb8e55
Tai <tmtudev@gmail.com>
2019-11-03 09:13:32 +0700

    update

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT,  "0.0.0.0", () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
+
+  const io = socketIO(server);
 
 
 // setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
@@ -22,13 +23,30 @@ var startFunction = setInterval(initRoom, 100);
 io.attach(4567);
 
 var joinedRoom;
-var rooms = {};
-var roomDatas = {};
+var rooms = [{
+interval: '',
+id: '', 
+isPlaying: false,
+isExecuting: false,
+scores: [{player: '', score: 0, correct: 0, wrong: 0}],  
+datas:[{player: '', slogan: ''}]
+
+}];
 
 
 var socket;
-var minRoom = 20000;
-var maxRoom = 100000;
+
+
+const minRoom = 20000;
+const maxRoom = 100000;
+const maxPlayer = 4;
+
+const roundDuration = 5000;
+const waitDuration = 3000;
+
+
+
+
 
 
 // function update(){
@@ -47,8 +65,12 @@ function initRoom(){
 	clearInterval(startFunction);
 
 	for(var i = minRoom; i < maxRoom; i++){
-		rooms[i] = 0;
-		// console.log("Room: " + i + " created!");
+		rooms[i] =  {};
+		
+		ResetRoom(i);
+		// console.log(rooms[i].datas);
+
+		console.log("Room: " + i + " created!");
 	}
 }
 
@@ -60,7 +82,6 @@ io.on('connection', function(socket){
 
 	socket.on("joinRoom", function(roomId){
 		console.log("received event from socket: " + socket.id);
-		roomDatas[socket.id] = roomId;
 		joinRoom(socket, roomId);
 	});
 
@@ -73,7 +94,6 @@ io.on('connection', function(socket){
 });
 
 
-const maxPlayer = 4;
 
 function join(socket, room){
 			socket.join(room, function(){
@@ -87,10 +107,52 @@ function join(socket, room){
 				//start game
 				console.log("room " + room + " starting game!");
 				socket.emit("startGame");
-
+				rooms[room]['isStart'] = true;
 			}
 		});
 			 
+}
+
+function leave(socket, room){
+	console.log("some one leave room " + room);
+	socket.leave(room);
+}
+
+function excecuteGames(){
+	for(var i =0; i < rooms.length; i++){
+		if(rooms[i]['isPlaying' && !rooms[i].isExecuting]){
+			//this room is playing, execute it.
+			rooms[i].isExecuting = true;
+			UpdateRoom(rooms[i]);
+		}
+	}
+
+}
+
+
+function ResetRoom(i){
+	rooms[i].id = i;
+	rooms[i].isPlaying = false;
+	rooms[i].isExecuting = false;
+	rooms[i].datas = [];
+	rooms[i].scores = [];
+}
+function UpdateRoom(room){
+	rooms[room.id].interval = room.setInterval(roundDuration, function(){
+		//pop data room
+		var data = room.datas.pop();
+		io.to(room.id).emit('onGameUpdate', room);
+		if(datas.length <= 0)
+		{
+			// clearInterval(rooms[room.id].interval);
+			//complete game, clear room data and status, kick all players.
+			io.to(room.id).emit('onGameCompleted', room);
+			ResetRoom(room.id);
+		}
+		else{
+			UpdateRoom(room);
+		}
+	});
 }
 
 function joinRoom(socket, room){
